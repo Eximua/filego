b381927d8f75125d4c30490744bb3122347f4ead
ivy <597055914@qq.com>
2018-08-05 18:38:26 +0800

    [feature] Add Distributed Hash Tables

--- main.js
+++ main.js
@@ -15,10 +15,26 @@ function print (protocol, conn) {
     )
 }
 
+// parallel([
+//     (cb) => createNode(['/ip4/0.0.0.0/tcp/0', '/ip4/127.0.0.1/tcp/10000/ws'], cb),
+//     (cb) => createNode(['/ip4/0.0.0.0/tcp/0', '/ip4/127.0.0.1/tcp/20000/ws'], cb),
+//   ], (err, nodes) => {
+//     if (err) { throw err }
+  
+//     const node1 = nodes[0]
+//     const node2 = nodes[1]
+  
+//     printAddrs(node1, '1')
+//     printAddrs(node2, '1')
+
+//     node1.on('peer:discovery', (peer) => console.log('Discovered:', peer.id.toB58String()))
+//     node2.on('peer:discovery', (peer) => console.log('Discovered:', peer.id.toB58String()))
+// })
+
 parallel([
-    (cb) => createNode('/ip4/0.0.0.0/tcp/0', cb),
-    (cb) => createNode(['/ip4/0.0.0.0/tcp/0', '/ip4/127.0.0.1/tcp/10000/ws'], cb),
-    (cb) => createNode('/ip4/127.0.0.1/tcp/20000/ws', cb)
+    (cb) => createNode(['/ip4/0.0.0.0/tcp/0', '/ip4/127.0.0.1/tcp/10000/ws'],cb),
+    (cb) => createNode(['/ip4/0.0.0.0/tcp/0', '/ip4/127.0.0.1/tcp/20000/ws'],cb),
+    (cb) => createNode(['/ip4/0.0.0.0/tcp/0', '/ip4/127.0.0.1/tcp/30000/ws'],cb)
   ], (err, nodes) => {
     if (err) { throw err }
   
@@ -26,32 +42,19 @@ parallel([
     const node2 = nodes[1]
     const node3 = nodes[2]
   
-    printAddrs(node1, '1')
-    printAddrs(node2, '2')
-    printAddrs(node3, '3')
-  
-    node1.handle('/print', print)
-    node2.handle('/print', print)
-    node3.handle('/print', print)
-  
-    // node 1 (TCP) dials to node 2 (TCP+WebSockets)
-    node1.dialProtocol(node2.peerInfo, '/print', (err, conn) => {
+    parallel([
+      (cb) => node1.dial(node2.peerInfo, cb),
+      (cb) => node2.dial(node3.peerInfo, cb),
+      // Set up of the cons might take time
+      (cb) => setTimeout(cb, 300)
+    ], (err) => {
       if (err) { throw err }
   
-      pull(pull.values(['node 1 dialed to node 2 successfully']), conn)
-    })
+      node1.peerRouting.findPeer(node3.peerInfo.id, (err, peer) => {
+        if (err) { throw err }
   
-    // node 2 (TCP+WebSockets) dials to node 2 (WebSockets)
-    node2.dialProtocol(node3.peerInfo, '/print', (err, conn) => {
-      if (err) { throw err }
-  
-      pull(pull.values(['node 2 dialed to node 3 successfully']), conn)
+        console.log('Found it, multiaddrs are:')
+        peer.multiaddrs.forEach((ma) => console.log(ma.toString()))
+      })
     })
-  
-    // node 3 (WebSockets) attempts to dial to node 1 (TCP)
-    node3.dialProtocol(node1.peerInfo, '/print', (err, conn) => {
-      if (err) {
-        console.log('node 3 failed to dial to node 1 with:', err.message)
-      }
-    })
-})
+  })
--- src/p2p/bundle.js
+++ src/p2p/bundle.js
@@ -3,6 +3,22 @@ const TCP = require('libp2p-tcp');
 const WebSockets = require('libp2p-websockets');
 const defaultsDeep = require('@nodeutils/defaults-deep');
 const SPDY = require('libp2p-spdy');
+const SECIO = require('libp2p-secio');
+const Bootstrap = require('libp2p-railing');
+const MulticastDNS = require('libp2p-mdns');
+const kadDHT = require('libp2p-kad-dht');
+
+const bootstrapers = [
+    '/ip4/104.131.131.82/tcp/4001/ipfs/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
+    '/ip4/104.236.176.52/tcp/4001/ipfs/QmSoLnSGccFuZQJzRadHn95W2CrSFmZuTdDWP8HXaHca9z',
+    '/ip4/104.236.179.241/tcp/4001/ipfs/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM',
+    '/ip4/162.243.248.213/tcp/4001/ipfs/QmSoLueR4xBeUbY9WZ9xGUUxunbKWcrNFTDAadQJmocnWm',
+    '/ip4/128.199.219.111/tcp/4001/ipfs/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu',
+    '/ip4/104.236.76.40/tcp/4001/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64',
+    '/ip4/178.62.158.247/tcp/4001/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd',
+    '/ip4/178.62.61.185/tcp/4001/ipfs/QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3',
+    '/ip4/104.236.151.122/tcp/4001/ipfs/QmSoLju6m7xTh3DuokvT3886QRYqxAzb1kShaanJgW36yx'
+]
 
 class FilegoBundles extends libp2p {
     constructor (_options) {
@@ -13,8 +29,30 @@ class FilegoBundles extends libp2p {
             WebSockets
           ],
 
-          streamMuxer: [ SPDY ]
-        }
+          streamMuxer: [ SPDY ],
+
+          connEncryption: [ SECIO ],
+
+        //   peerDiscovery: [ MulticastDNS ],
+
+          dht: kadDHT
+        },
+        config: {
+        //   peerDiscovery: {
+        //     bootstrap: {
+        //       interval: 2000,
+        //       enabled: true
+        //     }
+        //   },
+
+          dht: {
+            kBucketSize: 20
+          },
+
+          EXPERIMENTAL: {
+            dht: true
+          }          
+        }        
       }
   
       super(defaultsDeep(_options, defaults))
